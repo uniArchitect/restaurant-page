f685febfc5daa1da715d8d8cd0f3d3bb0b8f063e
uniArchitect <hancho93@gmail.com>
2022-07-26 20:39:12 -0400

    FEAT: background image introduced to cover page

---
+++ src/index.js
@@ -0,0 +1,14 @@
+// Home Page
+import Cover_Image from './images/food-cover.jpg';
+
+const TAB_CONTAINER_DIV = document.createElement('div');
+const CONTAINER_DIV = document.createElement('div');
+
+document.body.appendChild(TAB_CONTAINER_DIV).className = 'tab-container';
+document.body.appendChild(CONTAINER_DIV).className = 'body-container';
+
+// Add cover image
+const COVER_IMAGE = new Image();
+COVER_IMAGE.src = Cover_Image;
+
+CONTAINER_DIV.appendChild(COVER_IMAGE);
