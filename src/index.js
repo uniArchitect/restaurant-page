dc00d0f19e0652bbb725bb6d2157b79c57250a27
uniArchitect <hancho93@gmail.com>
2022-07-27 22:05:32 -0400

    STYLE: flex style for submenu to properly gap links. font styles to account for active and hover states.

--- src/index.js
+++ src/index.js
@@ -6,15 +6,20 @@ import Cover_Image from './images/boba-cover.jpg';
 const HEADER_DIV = document.createElement('div');
 const CONTAINER_DIV = document.createElement('div');
 
-// Header links
+// Header submenu
 const NAV_MENU_UL = document.createElement('ul');
-const NAV_SUBMENU_LI = document.createElement('li');
+const NAV_HOURS_LOCATION_LI = document.createElement('li');
+const NAV_MENUS_LI = document.createElement('li');
+const NAV_ABOUT_LI = document.createElement('li');
+const NAV_PRIVATE_EVENTS_LI = document.createElement('li');
+const NAV_LOGO_DIV = document.createElement('div');
 const HOURS_LOCATION_A = document.createElement('a');
 const MENUS_A = document.createElement('a');
 const ABOUT_A = document.createElement('a');
 const PRIVATE_EVENTS_A = document.createElement('a');
 
 // Submenu link HTML
+NAV_LOGO_DIV.innerHTML = 'INITIAL TEA SHOP'
 HOURS_LOCATION_A.innerHTML = 'LOCATION'
 MENUS_A.innerHTML = 'MENU'
 ABOUT_A.innerHTML = 'ABOUT'
@@ -35,12 +40,20 @@ document.body.appendChild(HEADER_DIV).className = 'header';
 document.body.appendChild(CONTAINER_DIV).className = 'body-container';
 
 // Append links to header div
-HEADER_DIV.appendChild(NAV_MENU_UL);
-NAV_MENU_UL.appendChild(NAV_SUBMENU_LI);
-NAV_SUBMENU_LI.appendChild(HOURS_LOCATION_A).className = 'submenu-link';
-NAV_SUBMENU_LI.appendChild(MENUS_A).className = 'submenu-link';
-NAV_SUBMENU_LI.appendChild(ABOUT_A).className = 'submenu-link';
-NAV_SUBMENU_LI.appendChild(PRIVATE_EVENTS_A).className = 'submenu-link';
+HEADER_DIV.appendChild(NAV_MENU_UL).className = 'nav-menu';
+HEADER_DIV.appendChild(NAV_LOGO_DIV).className = 'nav-logo';
+
+NAV_MENU_UL.appendChild(NAV_HOURS_LOCATION_LI);
+NAV_HOURS_LOCATION_LI.appendChild(HOURS_LOCATION_A).className = 'submenu-link';
+
+NAV_MENU_UL.appendChild(NAV_MENUS_LI);
+NAV_MENUS_LI.appendChild(MENUS_A).className = 'submenu-link';
+
+NAV_MENU_UL.appendChild(NAV_ABOUT_LI);
+NAV_ABOUT_LI.appendChild(ABOUT_A).className = 'submenu-link';
+
+NAV_MENU_UL.appendChild(NAV_PRIVATE_EVENTS_LI);
+NAV_PRIVATE_EVENTS_LI.appendChild(PRIVATE_EVENTS_A).className = 'submenu-link';
 
 // Append banner image to homepage
 CONTAINER_DIV.appendChild(COVER_IMAGE);
