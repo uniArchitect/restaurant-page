009e0b4a0448484a84852f2b5bac51eda0f22852
uniArchitect <hancho93@gmail.com>
2022-08-01 20:43:36 -0400

    STYLE: add business hours grid display and styles

--- src/about.js
+++ src/about.js
@@ -14,6 +14,26 @@ const HOURS_HEADER = document.createElement('div')
 const HOURS_DAYS = document.createElement('div')
 const HOURS_TIME = document.createElement('div')
 
+// List of Hours of Operation
+const DAYS_UL = document.createElement('ul')
+const SUNDAY_LI = document.createElement('li')
+const MONDAY_LI = document.createElement('li')
+const TUESDAY_LI = document.createElement('li')
+const WEDNESDAY_LI = document.createElement('li')
+const THURSDAY_LI = document.createElement('li')
+const FRIDAY_LI = document.createElement('li')
+const SATURDAY_LI = document.createElement('li')
+
+const TIME_UL = document.createElement('ul')
+const SUNDAY_TIME = document.createElement('li')
+const MONDAY_TIME = document.createElement('li')
+const TUESDAY_TIME = document.createElement('li')
+const WEDNESDAY_TIME = document.createElement('li')
+const THURSDAY_TIME = document.createElement('li')
+const FRIDAY_TIME = document.createElement('li')
+const SATURDAY_TIME = document.createElement('li')
+
+
 const ABOUT_COVER = new Image();
 ABOUT_COVER.src = About_Cover;
 
@@ -48,8 +68,29 @@ const aboutCreate = () => {
     // Hours of Operation
     ABOUT_CONTAINER_DIV.appendChild(ABOUT_HOURS_DIV).className = 'about-hours'
     ABOUT_HOURS_DIV.appendChild(HOURS_HEADER).className = 'hours-header'
-    HOURS_HEADER.appendChild(HOURS_DAYS).className = 'hours-days'
-    HOURS_HEADER.appendChild(HOURS_TIME).className = 'hours-time'
+    HOURS_HEADER.innerHTML = 'BUSINESS HOURS'
+
+    ABOUT_HOURS_DIV.appendChild(HOURS_DAYS).className = 'hours-days'
+    HOURS_DAYS.appendChild(DAYS_UL).className = 'day-list'
+    DAYS_UL.append(SUNDAY_LI, MONDAY_LI, TUESDAY_LI, WEDNESDAY_LI, THURSDAY_LI, FRIDAY_LI, SATURDAY_LI)
+    SUNDAY_LI.innerHTML = 'SUNDAY'
+    MONDAY_LI.innerHTML = 'MONDAY'
+    TUESDAY_LI.innerHTML = 'TUESDAY'
+    WEDNESDAY_LI.innerHTML = 'WEDNESDAY'
+    THURSDAY_LI.innerHTML = 'THURSDAY'
+    FRIDAY_LI.innerHTML = 'FRIDAY'
+    SATURDAY_LI.innerHTML = 'SATURDAY'
+
+    ABOUT_HOURS_DIV.appendChild(HOURS_TIME).className = 'hours-time'
+    HOURS_TIME.appendChild(TIME_UL).className = 'time-list'
+    TIME_UL.append(SUNDAY_TIME, MONDAY_TIME, TUESDAY_TIME, WEDNESDAY_TIME, THURSDAY_TIME, FRIDAY_TIME, SATURDAY_TIME)
+    SUNDAY_TIME.innerHTML = '12 PM - 8 PM'
+    MONDAY_TIME.innerHTML = '12 PM - 8 PM'
+    TUESDAY_TIME.innerHTML = '12 PM - 8 PM'
+    WEDNESDAY_TIME.innerHTML = '12 PM - 8 PM'
+    THURSDAY_TIME.innerHTML = '12 PM - 8 PM'
+    FRIDAY_TIME.innerHTML = '12 PM - 8 PM'
+    SATURDAY_TIME.innerHTML = '12 PM - 8 PM'
 }
 
 export { ABOUT_CONTAINER_DIV, aboutCreate };
